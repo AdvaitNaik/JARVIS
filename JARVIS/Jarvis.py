bfe925732eab274e6ded6606408ca34dc1fc37b4
Lakshay Mahajan <69849425+LakshayMahajan2006@users.noreply.github.com>
2022-10-01 00:03:10 +0530

    Update Jarvis.py

--- JARVIS/Jarvis.py
+++ JARVIS/Jarvis.py
@@ -1,172 +1,180 @@
 import pyttsx3
 import datetime
 import speech_recognition as sr
+import pyaudio
 import wikipedia
-import smtplib
-import webbrowser as wb
+import webbrowser
 import os
-import pyautogui
-import psutil
-import pyjokes
+import smtplib
+import sys
+
+
+MASTER = "Sir"
+
 
 engine = pyttsx3.init()
+
 
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
 
-# speak("This is Jarvis Adik")
 
 def time():
-    Time = datetime.datetime.now().strftime("%I:%M:%S")
-    speak("The current time is")
+    Time = datetime.datetime.now().strftime("%I:%M:%S" )
     speak(Time)
 
-# time()
 
 def date():
-    year = int(datetime.datetime.now().year)
-    month = int(datetime.datetime.now().month)
-    date = int(datetime.datetime.now().day)
-    speak("The current date is")
+    year = int(datetime.datetime.now().year )
+    month = int(datetime.datetime.now().month )
+    date = int(datetime.datetime.now().day )
     speak(date)
     speak(month)
     speak(year)
 
-# date()
 
-def wishme():
-    speak("Welcome back Advait Sir")
+def wishMe():
+    speak("Welcome back sir")
     # speak("The current time is")
-    time()
+    # time()
     # speak("The current date is")
-    date()
+    # date()
     hour = datetime.datetime.now().hour
-    if hour >= 6 and hour<12:
-        speak("Good morning sir!")
-    elif hour >=12 and hour<=18:
-        speak("Good afternoon sir!")
-    elif hour >=18 and hour<=24:
-        speak("Good evening sir!")
+    if hour>=0 and hour<12:
+        speak("Good Morning " + MASTER)
+
+
+    elif hour>=12 and hour<18:
+        speak("Good Afternoon " + MASTER)
+
+
     else:
-        speak("Good night sir!")
+        speak("Good Evening " + MASTER)
+    speak("I am JARVIS. Please tell me sir how may I help you?")
 
-    speak("Jarvis at your service Please tell me how can I help you?")
 
-# wishme()
-    
 def takeCommand():
     r = sr.Recognizer()
     with sr.Microphone() as source:
-        print("Listening....")
+        print("Listening......")
         r.pause_threshold = 1
         audio = r.listen(source)
 
+
     try:
-        print("recognizing....")
-        query = r.recognize_google(audio,language='en-in')
-        print(query)
+        print("Recognising.....")
+        query = r.recognize_google(audio, language='en-in')
+        print("Sir you said: ", query)
+
 
     except Exception as e:
         print(e)
-        speak("Say that again please...")
+        speak("Say that again please.....")
 
-        return "None"
+        return "None"   
     return query
 
-# takeCommand()
-
-def sendEmail(to, content):
-    server = smtplib.SMTP('smtp.gmail.com',587)
-    server.ehlo()
-    server.starttls()
-    server.login('abcd@gmail.com','123')
-    server.sendmail('abcd@gmail.com', to, content)
-    server.close()
-
-def screenshot():
-    img = pyautogui.screenshot()
-    img.save("F:\\ADVAIT\\UDEMY\\JARVIS\\JARVIS\\ss.png")
-
-def cpu():
-    usage = str(psutil.cpu_percent())
-    speak("CPU is at"+usage)
-    battery = psutil.sensors_battery()
-    speak("Battery is at")
-    speak(battery.percent)
-
-def jokes():
-    speak(pyjokes.get_joke())
 
 if __name__ == "__main__":
-    wishme()
+    wishMe()
     while True:
+    # if 2:
         query = takeCommand().lower()
-        if 'time' in query:
+        if 'wikipedia' in query:
+            speak('Searching Wikipedia.....')
+            query = query.replace("Wikipedia","")
+            results =wikipedia.summary(query, sentences=2)
+            speak("According to wikipedia")
+            print(results)
+            speak(results)
+
+
+        elif 'open youtube' in query:
+            webbrowser.open("youtube.com")
+
+
+        elif 'open google' in query:
+            webbrowser.open("google.com")
+
+
+        elif 'open stackoverflow' in query:
+            webbrowser.open("stackoverflow.com")
+
+
+        elif 'anaconda' in query:
+            anaconda = 'C:\\Desktop\\Anaconda Navigator (anaconda3).lnk'
+
+
+        elif 'time' in query:
+            strTime = datetime.datetime.now().strftime("%I:%M:%S")
+            speak("Sir the time is" + strTime)
+
+
+        elif 'gmail' in query:
+            webbrowser.open("gmail.com")
+
+
+        elif 'code' in query:
+            path = "C:\\Users\\sai\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
+            os.startfile(path)
+
+
+        elif 'chess' in query:
+            gamePath = "C:\\Program Files\\Microsoft Games\\Chess\\chess.exe"
+            os.startfile(gamePath)
+
+
+        elif 'purble' in query:
+            path = "C:\\Program Files\\Microsoft Games\\Purble Place\\PurblePlace.exe"
+
+
+        elif 'idle' in query:
+            path = "C:\\Users\\sai\\AppData\\Local\\Programs\\Python\\Python38-32\\Lib\\idlelib\\idle.pyw"
+
+
+        elif 'time' in query:
+            speak("The current time is")
             time()
+
+
         elif 'date' in query:
+            speak("The current date is")
             date()
-        elif 'wikipedia' in query:
-            speak("Searching.....")
-            query = query.replace("wikipedia","")
-            result = wikipedia.summary(query, sentences=2)
-            print(result)
-            speak(result)   
-        elif 'send email' in query:
-            try:
-                speak("What should I say?")
-                content - takeCommand()
-                to = "xyz@gmail.com"
-                sendEmail(to,content)
-                speak("Email has been sent!")
-            except Exception as e:
-                print(e)
-                speak("Unable to send email")
 
-        elif 'search in chrome' in query:
-            speak('What should i search?')
-            chromepath = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
-            search = takeCommand().lower()
-            wb.get(chromepath).open_new_tab(search+'.com')
-  
-        elif 'logout' in query:
-            os.system("shutdown -l")
 
-        elif 'shoutdown' in query:
-            os.system("shutdown /s /t l")
+        elif 'hello' in query:
+            speak("Hello sir. How are you?")
+            print("Hello sir. How are you?")
 
-        elif 'restart' in query:
-            os.system("shutdown /r /t l")
 
-        elif 'play songs' in query:
-            songs_dir = 'D:\\Music'
-            songs = os.listdir(songs_dir)
-            os.sartfile(os.path.join(songs_dir,songs[0]))
+        elif 'I am fine' in query:
+            speak("Ok, Sir.")
+            print("Ok, Sir.")
 
-        elif 'remember that' in query:
-            speak("What should I remember?")
-            data = takeCommand()
-            speak("you said me to remember that"+data)
-            remember = open('data.txt','w')
-            remember.write(data)
-            remember.close()
 
-        elif 'do you know anything' in query:
-            remember = open('data.txt','r')
-            speak("you said me to remember that"+remember.read())
+        elif 'I am not well' in query:
+            print("Don't use laptop at this time.")
+            speak("Don't use laptop at this time.")
 
-        elif 'screenshot' in query:
-            screenshot()
-            speak("Done!")
 
-        elif 'cpu' in query:
-            cpu()
-
-        elif 'joke' in query:
-            jokes()
-
-        elif 'offline' in query:
-            quit()    
+        elif 'shutdown' in query:
+            print("1. Shutdown Computer")
+            print("2. Restart Computer")
+            print("3. Exit")
+            choice = int(input("\nEnter your choice: "))
+            if(choice>=1 and choice<=2):
+                if choice == 1:
+                    os.system("shutdown /s /t 1")
+                else:
+                    os.system("shutdown /r /t 1")
+            else:
+                exit()
         
 
+        elif 'song1' in query:
+            location = 'C:\\mp3\\audio\\Chal_Oye_Official_Video_Parmish_Verma___Desi_Cre.mp3'
 
+
+        elif 'quit' in query:
+            sys.exit()
